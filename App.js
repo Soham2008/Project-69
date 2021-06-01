c3eb75ca8f58a910831ce47e1d9382e94e1a3a27
Soham2008 <sohamswarup2008@gmail.com>
2021-06-01 16:31:35 +0530

    Created a new Expo app

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import TransactionScreen from './screens/BookTransactionScreen'
+import SearchScreen from './screens/BookTransactionScreen'
+import { createAppContainer } from 'react-navigation';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+});
